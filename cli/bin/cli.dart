06e6e14b63b7f9ef1c3ca79dfd7830af1887fda2
KHELIFA Yassine <prostam27@gmail.com>
2021-09-09 22:16:36 +0200

    var vs dynamic

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -2,4 +2,16 @@ import 'package:cli/cli.dart' as cli;
 
 void main(List<String> arguments) {
   print('${cli.hello()}: ${cli.calculate()}!');
+
+  String valueStr = "Value type String";
+  var value = " var fix text";
+  dynamic valueDym = "can u change type value";
+
+  valueStr = "Amine";
+  value = "Moha";
+  valueDym = 2021;
+
+  print(valueDym);
+  print(value);
+  print(valueStr);
 }
