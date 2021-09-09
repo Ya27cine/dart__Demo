3f08469f414536fc8ea5af38f17458ace8cffb6d
KHELIFA Yassine <prostam27@gmail.com>
2021-09-09 22:40:43 +0200

    Const VS Final

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,17 +1,16 @@
 import 'package:cli/cli.dart' as cli;
 
 void main(List<String> arguments) {
-  print('${cli.hello()}: ${cli.calculate()}!');
+  // print('${cli.hello()}: ${cli.calculate()}!');
 
-  String valueStr = "Value type String";
-  var value = " var fix text";
-  dynamic valueDym = "can u change type value";
+  const pi = 3.14;
+  final _count;
 
-  valueStr = "Amine";
-  value = "Moha";
-  valueDym = 2021;
+  /*..
 
-  print(valueDym);
-  print(value);
-  print(valueStr);
+   ..*/
+
+  _count = 77;
+
+  print(pi);
 }
