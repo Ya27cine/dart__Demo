b0059bc5a5173ec8e0f34a69c7df4df62f7f4075
KHELIFA Yassine <prostam27@gmail.com>
2021-09-09 23:29:36 +0200

    IF Statement Boolean in Dart

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,15 +1,9 @@
 import 'package:cli/cli.dart' as cli;
 
 void main(List<String> arguments) {
-  // print('${cli.hello()}: ${cli.calculate()}!');
+  String name = '';
 
-  const courses = ["Python", "Java", "C++", "Dart", "Go", "Perl", "Luna"];
-  final framework = ["Laravel", "Spring boot"];
-
-  //courses[2] = "JavaScript";
-  // error ; Cannot modify an unmodifiable list
-
-  framework[1] = "Symfony 4";
-
-  print(framework);
+  if (name.isEmpty) {
+    print("Please tape your naem .");
+  }
 }
