560aea1e2c38b73eada4eb7928a93b67fba86e0a
KHELIFA Yassine <prostam27@gmail.com>
2021-09-09 22:48:48 +0200

    Const VS Final part 2

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -3,14 +3,13 @@ import 'package:cli/cli.dart' as cli;
 void main(List<String> arguments) {
   // print('${cli.hello()}: ${cli.calculate()}!');
 
-  const pi = 3.14;
-  final _count;
+  const courses = ["Python", "Java", "C++", "Dart", "Go", "Perl", "Luna"];
+  final framework = ["Laravel", "Spring boot"];
 
-  /*..
+  //courses[2] = "JavaScript";
+  // error ; Cannot modify an unmodifiable list
 
-   ..*/
+  framework[1] = "Symfony 4";
 
-  _count = 77;
-
-  print(pi);
+  print(framework);
 }
